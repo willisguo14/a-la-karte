22d0d8bfd88fae853ff096e5b9991b114005bb8e
jdeng1040 <73567329+jdeng1040@users.noreply.github.com>
2021-07-14 20:48:35 -0400

    created signOut

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import firebase from 'firebase'
 
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 
 export default function Profile() {
     return (
         <View>
             <Text>You're a seller</Text>
+            <TouchableOpacity onPress={() => firebase.auth().signOut()}>
+                <Text>
+                    Sign out
+                </Text>
+            </TouchableOpacity>
         </View>
     )
 }
--- components/main/ProfileFalse.js
+++ components/main/ProfileFalse.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import firebase from 'firebase'
 
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 
 export default function ProfileFalse() {
     return (
         <View>
             <Text>You're not a seller</Text>
+            <TouchableOpacity onPress={() => firebase.auth().signOut()}>
+                <Text style={styles.signOut}>
+                    Sign out
+                </Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    signOut: {
+        fontSize: 40,
+        marginTop: 20
+    }
+  });
